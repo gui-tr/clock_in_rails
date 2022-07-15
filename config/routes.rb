9377fc905cfd3591d3c24b23ff0f7bb105002877
Gui Tri <guillaume.trionfo@gmail.com>
2022-07-15 20:44:47 +0100

    test route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :shifts
   #get 'home/index'
   get 'home/about'
+  get 'users/sign_in'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
